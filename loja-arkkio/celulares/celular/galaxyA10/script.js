2d6ce456baec5c38cb0f0d6a2e2cd283fe26d6bd
Erick Balbino da Silva <erickbaddog2@gmail.com>
2021-04-16 13:11:16 -0300

    update index.html

---
+++ loja-arkkio/celulares/celular/galaxyA10/script.js
@@ -0,0 +1,8 @@
+function mudarCorAzul(){
+    let foto = document.getElementById('img')
+    foto.src = 'galaxy-a10s.jpeg'
+}
+function mudarCorVermelha(){
+    let foto = document.getElementById('img')
+    foto.src = 'galaxya10s-vermelho.jpeg'
+}
