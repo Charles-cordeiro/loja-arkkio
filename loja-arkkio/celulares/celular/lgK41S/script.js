30d08d1ecc2bf5e004edec47c77065bb84be899a
Erick Balbino da Silva <erickbaddog2@gmail.com>
2021-04-18 15:08:30 -0300

    updtate lgk41s.html

---
+++ loja-arkkio/celulares/celular/lgK41S/script.js
@@ -0,0 +1,8 @@
+function mudarFrente(){
+    let foto = document.getElementById('img')
+    foto.src = '../../imagens/lg-41s-preto.jpeg'
+}
+function mudarCosta(){
+    let foto = document.getElementById('img')
+    foto.src = '../../imagens/lgk41s-costa.jpeg'
+}
