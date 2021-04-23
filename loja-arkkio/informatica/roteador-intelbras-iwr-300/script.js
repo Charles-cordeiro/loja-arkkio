805ac65bc319e30d9700956cd3125243548ea4da
Erick Balbino da Silva <erickbaddog2@gmail.com>
2021-04-23 01:28:24 -0300

    update informatica.html

---
+++ loja-arkkio/informatica/roteador-intelbras-iwr-300/script.js
@@ -0,0 +1,8 @@
+var foto = document.getElementById("img");
+function mudarFrente(){
+    foto.src='roteador-intelbras-IWR-300.jpeg';
+}
+
+function mudarCostas() {
+    foto.src='roteador-intelbras-IWR-300-costas.jpeg'
+}
