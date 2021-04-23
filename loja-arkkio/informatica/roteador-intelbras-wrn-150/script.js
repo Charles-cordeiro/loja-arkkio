3d9ef34f9adf98938700349e978b5f35a65a7a2d
Erick Balbino da Silva <erickbaddog2@gmail.com>
2021-04-23 13:45:28 -0300

    update index.html

---
+++ loja-arkkio/informatica/roteador-intelbras-wrn-150/script.js
@@ -0,0 +1,9 @@
+var foto = document.querySelector('.img #img');
+
+function mudarFrente() {
+    foto.src='roteador-intelbras-WRN-150.jpeg';
+}
+
+function mudarCostas() {
+      foto.src ='roteador-intelbras-WRN-150-costas.jpeg';
+}
