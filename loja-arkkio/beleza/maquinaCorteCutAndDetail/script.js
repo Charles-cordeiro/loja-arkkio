37d1a1b0eefccba6ac561edd2f22193a36765d65
Erick Balbino da Silva <erickbaddog2@gmail.com>
2021-05-11 00:50:48 -0300

    update all files

---
+++ loja-arkkio/beleza/maquinaCorteCutAndDetail/script.js
@@ -0,0 +1,12 @@
+function mudarImg1(){
+    let foto = document.getElementById('img')
+    foto.src = 'caixa-maquina-de-corte-wahl-cut-and-detail.jpeg'
+}
+function mudarImg2(){
+    let foto = document.getElementById('img')
+    foto.src = 'maquina-wahl-cut-and deital-aparador-de-cabelo.jpg'
+}
+function mudarImg3(){
+    let foto = document.getElementById('img')
+    foto.src = 'imagemEmbalagem.webp'
+}
---
+++ loja-arkkio/ouvidoria/contato.php
@@ -0,0 +1,34 @@
+<?php
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $mensagem = $_POST['txtouvidoria'];
+
+    $to = "suporte@arkkio.com.br";
+    $subject = "Ouvidoria - Loja Arkkio";
+    $body = "Nome: $nome \nEmail: $email \nMensagem: $mensagem";
+    $header = "From: $email";
+
+    mail($to, $subject, $body, $header);
+
+?>
+
+<p>Email enviado com sucesso!</p>
+<a href="../../index.html"><input type="submit" value="Voltar"></a>
+
+<style>
+    body{
+        background-color: #f5f5f5;
+    }
+    p{
+        font-size: 1.1rem;
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    }
+    input[type = "submit"]{
+        font-size = 1.2rem;
+        background-color: #fa605d;
+        color: #fff;
+        border: none;
+        padding: 3px 8px;
+        cursor: pointer;
+    }
+</style>
